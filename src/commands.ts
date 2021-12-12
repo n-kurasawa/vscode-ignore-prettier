a9f27f709eec42a13254917323b78a1e4f04ae56
naohiro.kurasawa <naohiro.kurasawa@gunosy.com>
2021-12-12 15:43:47 +0900

    add command

--- src/commands.ts
+++ src/commands.ts
@@ -1,5 +1,23 @@
-import { window } from "vscode";
+import { window, workspace, Uri } from "vscode";
+import { TextEncoder } from "util";
 
 export const add = () => {
-  window.showInformationMessage("ADD");
+  const folders = workspace.workspaceFolders;
+  if (folders === undefined || folders.length > 1) {
+    console.error(`need a folder.`);
+    return;
+  }
+
+  const editor = window.activeTextEditor;
+  if (!editor) {
+    console.error(`need active text editor.`);
+    return;
+  }
+
+  const uri = Uri.file(`${folders[0].uri.path}/.prettierignore`);
+  const filename = editor.document.fileName.replace(
+    `${folders[0].uri.path}/`,
+    ""
+  );
+  workspace.fs.writeFile(uri, new TextEncoder().encode(filename));
 };
