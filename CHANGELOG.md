f223103b01cf35150e2738836ce55cc50e089996
naohiro.kurasawa <naohiro.kurasawa@gunosy.com>
2021-12-13 07:50:04 +0900

    rename project

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Change Log
 
-All notable changes to the "vscode-prettierignore" extension will be documented in this file.
+All notable changes to the "vscode-ignore-prettier" extension will be documented in this file.
 
 Check [Keep a Changelog](http://keepachangelog.com/) for recommendations on how to structure this file.
 
--- src/extension.ts
+++ src/extension.ts
@@ -2,11 +2,11 @@ import { commands, ExtensionContext } from "vscode";
 import { add, remove } from "./commands";
 
 export function activate(context: ExtensionContext) {
-  console.log('Extension "vscode-prettierignore" is activated.');
+  console.log('Extension "vscode-ignore-prettier" is activated.');
 
-  const addCommand = commands.registerCommand("prettierignore.add", add);
+  const addCommand = commands.registerCommand("ignoreprettier.add", add);
   const removeCommand = commands.registerCommand(
-    "prettierignore.remove",
+    "ignoreprettier.remove",
     remove
   );
 
