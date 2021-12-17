adac252c335941fe3f0c98fb66fd68654398eeb5
naohiro.kurasawa <naohiro.kurasawa@gunosy.com>
2021-12-17 22:06:17 +0900

    extract methods

--- src/commands.ts
+++ src/commands.ts
@@ -1,11 +1,55 @@
-import { window, workspace, FileSystemError } from "vscode";
+import {
+  window,
+  workspace,
+  FileSystemError,
+  WorkspaceFolder,
+  Uri,
+  TextEditor,
+} from "vscode";
 import { posix, sep } from "path";
 import { EOL } from "os";
 import { removeFilename } from "./removeFilename";
 
-export const add = async () => {
+const getFolder = (): WorkspaceFolder | null => {
   const folders = workspace.workspaceFolders;
   if (folders === undefined || folders.length > 1) {
+    return null;
+  }
+  return folders[0];
+};
+
+const getPrettierignoreUri = (uri: Uri) => {
+  return uri.with({
+    path: posix.join(uri.path, ".prettierignore"),
+  });
+};
+
+const exists = async (uri: Uri): Promise<boolean> => {
+  try {
+    await workspace.fs.stat(uri);
+  } catch (e) {
+    if (e instanceof FileSystemError && e.code === "FileNotFound") {
+      return false;
+    }
+    throw e;
+  }
+  return true;
+};
+
+const extractFilenameFromRoodDir = (
+  editor: TextEditor,
+  folder: WorkspaceFolder
+) => {
+  return editor.document.fileName.replace(`${folder.uri.path}${sep}`, "");
+};
+
+const createIgnoreValue = (currentValue: string, filename: string) => {
+  return Buffer.from(currentValue + filename + EOL, "utf8");
+};
+
+export const add = async () => {
+  const folder = getFolder();
+  if (folder === null) {
     console.error(`need a folder.`);
     return;
   }
@@ -16,29 +60,16 @@ export const add = async () => {
     return;
   }
 
-  const folderUri = folders[0].uri;
-  const uri = folderUri.with({
-    path: posix.join(folderUri.path, ".prettierignore"),
-  });
+  const uri = getPrettierignoreUri(folder.uri);
 
   let currentValue = "";
-  try {
+  if (await exists(uri)) {
     const readData = await workspace.fs.readFile(uri);
     currentValue = Buffer.from(readData).toString("utf8").trimEnd() + EOL;
-  } catch (e) {
-    if (!(e instanceof FileSystemError && e.code === "FileNotFound")) {
-      throw e;
-    }
   }
 
-  const filename = editor.document.fileName.replace(
-    `${folderUri.path}${sep}`,
-    ""
-  );
-  workspace.fs.writeFile(
-    uri,
-    Buffer.from(currentValue + filename + EOL, "utf8")
-  );
+  const filename = extractFilenameFromRoodDir(editor, folder);
+  workspace.fs.writeFile(uri, createIgnoreValue(currentValue, filename));
 };
 
 export const remove = async () => {
