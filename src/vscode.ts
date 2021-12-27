58e7f216e67121ad5e9db627f535c8c1b1d7dbfe
naohiro.kurasawa <naohiro.kurasawa@gunosy.com>
2021-12-27 21:23:03 +0900

    create EditService / impl add

--- src/vscode.ts
+++ src/vscode.ts
@@ -77,6 +77,67 @@ const exists = async (uri: Uri): Promise<boolean> => {
   return true;
 };
 
+class EditService {
+  private folder: WorkspaceFolder;
+  private editor: TextEditor;
+  private statusBarItem: StatusBarItem;
+  constructor(statusBarItem: StatusBarItem) {
+    this.statusBarItem = statusBarItem;
+    const folders = workspace.workspaceFolders;
+    if (folders === undefined || folders.length > 1) {
+      throw new Error("");
+    }
+    this.folder = folders[0];
+    if (!window.activeTextEditor) {
+      throw new Error("");
+    }
+    this.editor = window.activeTextEditor;
+  }
+  async add() {
+    const filename = this.getOpenedFilename();
+    const uri = this.getPrettierignoreUri();
+    let currentValue = "";
+    if (await this.exists(uri)) {
+      const readData = await workspace.fs.readFile(uri);
+      currentValue = Buffer.from(readData).toString("utf8").trimEnd() + EOL;
+    }
+    const newValue = currentValue + filename + EOL;
+    workspace.fs.writeFile(uri, Buffer.from(newValue, "utf8"));
+    this.disableStatusBarItemText();
+  }
+  remove() {}
+  toggle() {}
+  private async exists(uri: Uri): Promise<boolean> {
+    try {
+      await workspace.fs.stat(uri);
+    } catch (e) {
+      if (e instanceof FileSystemError && e.code === "FileNotFound") {
+        return false;
+      }
+      throw e;
+    }
+    return true;
+  }
+  private getOpenedFilename(): string {
+    return this.editor.document.fileName.replace(
+      `${this.folder.uri.path}${sep}`,
+      ""
+    );
+  }
+  private getPrettierignoreUri = () => {
+    const uri = this.folder.uri;
+    return uri.with({
+      path: posix.join(uri.path, ".prettierignore"),
+    });
+  };
+  private disableStatusBarItemText(): void {
+    this.statusBarItem.text = disableText;
+  }
+  private enableStatusBarItemText(): void {
+    this.statusBarItem.text = enableText;
+  }
+}
+
 export class Vscode {
   constructor(private statusBarItem: StatusBarItem) {}
 
