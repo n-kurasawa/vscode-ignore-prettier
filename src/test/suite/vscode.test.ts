cd6b3f6844471330bd1ddfef73f8662cf9f43dd4
naohiro.kurasawa <naohiro.kurasawa@gunosy.com>
2022-01-01 11:03:00 +0900

    add test wip

--- src/test/suite/vscode.test.ts
+++ src/test/suite/vscode.test.ts
@@ -1,14 +1,40 @@
 import * as assert from "assert";
+import { Uri, workspace } from "vscode";
 import { Vscode } from "../../vscode";
 
+function addFolder(dir: string, name: string) {
+  const result = workspace.updateWorkspaceFolders(
+    workspace.workspaceFolders ? workspace.workspaceFolders.length : 0,
+    null,
+    {
+      uri: Uri.parse(dir),
+      name,
+    }
+  );
+}
+
 suite("vscode test", () => {
-  test("init", () => {
+  test("init no folder", () => {
     try {
       new Vscode();
       assert.fail();
     } catch (e) {
       if (e instanceof Error) {
         assert.strictEqual(e.message, "folder not open.");
+      } else {
+        assert.fail();
+      }
+    }
+  });
+
+  test("init exsits folder", () => {
+    addFolder("test", "folder");
+    try {
+      new Vscode();
+      assert.fail();
+    } catch (e) {
+      if (e instanceof Error) {
+        assert.strictEqual(e.message, "no active editor.");
       } else {
         assert.fail();
       }
