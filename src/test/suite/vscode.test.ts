0e5bdaf0762b358fa6aefd5dc7d036763a48b9d0
naohiro.kurasawa <naohiro.kurasawa@gunosy.com>
2022-01-06 10:04:06 +0900

    add exists test / refactor test

--- src/test/suite/vscode.test.ts
+++ src/test/suite/vscode.test.ts
@@ -1,30 +1,55 @@
 import * as assert from "assert";
+import { suite } from "mocha";
 import { commands, Uri, window } from "vscode";
 import { Vscode } from "../../vscode";
 
 suite("vscode test", () => {
-  test("init no active editor", async () => {
-    try {
-      new Vscode();
-      assert.fail();
-    } catch (e) {
-      if (e instanceof Error) {
-        assert.strictEqual(e.message, "no active editor.");
-      } else {
+  suite("no active editor", () => {
+    test("init", async () => {
+      try {
+        new Vscode();
         assert.fail();
+      } catch (e) {
+        if (e instanceof Error) {
+          assert.strictEqual(e.message, "no active editor.");
+        } else {
+          assert.fail();
+        }
       }
-    }
+    });
   });
 
-  test("init", async () => {
-    const file = `${process.cwd()}/test-target/test.js`;
-    await window.showTextDocument(Uri.parse(file));
-    try {
+  suite("has active editor", () => {
+    suiteSetup(async () => {
+      const file = `${process.cwd()}/test-target/test.js`;
+      await window.showTextDocument(Uri.parse(file));
+    });
+
+    suiteTeardown(async () => {
+      await commands.executeCommand("workbench.action.closeActiveEditor");
+    });
+
+    test("init", async () => {
+      try {
+        const vscode = new Vscode();
+        assert.ok(vscode);
+      } catch (e) {
+        assert.fail();
+      }
+    });
+
+    test("exists", async () => {
+      const file = `${process.cwd()}/test-target/test.js`;
       const vscode = new Vscode();
-      assert.ok(vscode);
-    } catch (e) {
-      assert.fail();
-    }
-    commands.executeCommand("workbench.action.closeActiveEditor");
+      const actual = await vscode.exists(Uri.parse(file));
+      assert.strictEqual(actual, true);
+    });
+
+    test("not exists", async () => {
+      const vscode = new Vscode();
+      const notexists = `${process.cwd()}/test-target/not-exists.js`;
+      const actual = await vscode.exists(Uri.parse(notexists));
+      assert.strictEqual(actual, false);
+    });
   });
 });
